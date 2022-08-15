954cb7dcee570cabfbe3d633ccee68b48ba98626
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-08-15 11:04:28 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HTTPSnippet from "httpsnippet";
+
+const snippet = new HTTPSnippet({
+  method: "GET",
+  url: "https://www.google.de",
+});
+</script>
+
+<template>
+  <div class="hello">Hello Nuxt {{ snippet }}!</div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 10rem;
+}
+</style>
